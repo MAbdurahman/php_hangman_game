411a69e4253561654c6d6cc80601840c7421e12c
MAbdurahman <mdbdrrhm@gmail.com>
2021-12-28 08:36:38 -0600

    Add functionality to select a random word from a text file

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+    $words = file("words.txt");
+
+    $word = rtrim($words[array_rand($words)]);
+
+    echo $word;
+    echo "<br>";
+
+    $wordLength = strlen($word);
+
+    echo $wordLength;
+    echo "<br>";
+
+    for($i = 1; $i <= $wordLength; $i++){
+        echo "_ ";
+    }
